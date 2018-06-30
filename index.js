b492aca04ea7ab585b26b4dfaa5ec8acddf9a915
chadwithuhc <chadwithuhc@gmail.com>
2018-06-30 00:04:53 -0600

    Tattling on build breakers

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
+const logParser = require('./lib/logParser')
+
+const fetch = require('node-fetch')
+
 /**
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
-  app.on('pull_request.opened', async context => {
-    // app.log('opened', context)
-    console.log('opened', context)
+  app.on('status', async context => {
+    if (context.payload.state === 'failure') {
+      console.log('FAIL!')
 
-    // pull_request.statuses_url
-    // https://api.github.com/repos/chadwithuhc/json-parser-code-challenge/statuses/7c698a7be5e60f93045e1151fd443265f4f9dd41
+      // https://travis-ci.org/chadwithuhc/json-parser-code-challenge/builds/398496342?utm_source=github_status&utm_medium=notification
+      const buildId = +(new URL(context.payload.target_url)).pathname.split(`/`).pop()
 
-    // GET https://api.github.com/repos/chadwithuhc/json-parser-code-challenge/statuses/f942ac6a0aa8a5c3b19ca7faae81fa79792b45b2
+      logParser(`https://api.travis-ci.org/v3/job/${buildId + 1}/log.txt`)
+        .then(log => {
+          const { commit, repository } = context.payload
 
-    // Response -> response[0].target_url
-    // https://travis-ci.org/chadwithuhc/json-parser-code-challenge/builds/398496342?utm_source=github_status&utm_medium=notification
+          // GET pulls
+          // match where pull.head.sha === commit.sha
+          fetch(repository.pulls_url.replace(`{/number}`, ``))
+            .then(res => res.json())
+            .then(pulls => {
+              const pull = pulls.find(pull => pull.head.sha === commit.sha)
 
-    // Extract ID -> 398496342 + 1
-    // GET https://api.travis-ci.org/v3/job/398496343/log.txt
+              if (!pull) {
+                console.log('ERR: Pull not found')
+                return
+              }
 
-    // Remove node terminal formatting
-  })
-  app.on('pull_request.synchronize', async context => {
-    // app.log('synchronize', context)
-    console.log('synchronize', context)
-  })
+              const comment = context.issue({
+                number: pull.number,
+                body: [
+                  `@${commit.author.login} broke the build!`,
+                  `<pre><code>${log}</code></pre>`
+                ].join(`\n\n`)
+              })
 
-  app.on('issues.opened', async context => {
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({ body: 'Hello World!' })
-
-    // Post a comment on the issue
-    return context.github.pullRequests.createComment(params)
+              return context.github.issues.createComment(comment)
+            })
+        })
+    }
   })
 }
